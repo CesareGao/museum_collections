74bdb5accac492ef3c52f667f4f6b353b5a971e6
Yan Gao <Yan.Gao@du.edu>
2019-03-13 01:13:27 -0600

    Adding credit_year function to the .py functions file

--- data_clean_functions.py
+++ data_clean_functions.py
@@ -1,3 +1,13 @@
 ## this file contain several functions for data cleaning of the dataset
 
-## 
+##
+def credit_year(row):
+    if pd.notna(row):
+        ## find the year in string type document
+        year_str = re.compile('\d{4}').findall(row.split(',')[-1])
+        if year_str:
+            year_int = int(year_str[0])
+            ## filter wrong years according to the found year of the museum
+            if 1870 <= year_int <= 2019:
+                return pd.Timestamp(str(year_int))
+    return np.nan
