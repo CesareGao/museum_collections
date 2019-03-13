3ed6a3de5cd0063e953c9e2b43c4aae0c3bb95e6
Yan Gao <Yan.Gao@du.edu>
2019-03-13 03:37:56 -0600

    Import required modules to the .py file

--- data_clean_functions.py
+++ data_clean_functions.py
@@ -1,6 +1,14 @@
 ## this file contain several functions for data cleaning of the dataset
 
-##
+## import modules
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from collections import Counter
+
+## data cleaning functions
+
 def credit_year(row):
     if pd.notna(row):
         ## find the year in string type document
